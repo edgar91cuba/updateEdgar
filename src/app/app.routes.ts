1cef42101d6e7dcbfc9eaecca9201137c1f8e57d
edgar91cuba <edgar91cuba@gmail.com>
2024-04-23 13:38:34 +0200

    Segundo Testeo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MapComponent } from './map/map.component';
+import { InfoComponent } from './info/info.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: HomeComponent,
+  },
+  { path: 'info',
+  component: InfoComponent
   },
   {
     path: '',
